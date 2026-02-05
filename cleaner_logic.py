ed79b65e11c92b90c49ede439f892fa6e774866e
AKASH OFFCICAL <mdakashmia755@gmail.com>
2026-02-06 00:35:05 +0600

    Update cleaner_logic.py

--- cleaner_logic.py
+++ cleaner_logic.py
@@ -7,19 +7,19 @@ def is_video_playing(url):
         return result.returncode == 0
     except: return False
 
-with open('database.json', 'r', encoding='utf-8') as f: db = json.load(f)
+try:
+    with open('database.json', 'r', encoding='utf-8') as f: db = json.load(f)
+except: exit()
 
 new_db, m3u = {}, "#EXTM3U\n"
 
 for name, links in db.items():
     active_links = []
     for item in links:
-        if is_video_playing(item['link']): # ডেড লিংক ফিল্টার করা
+        if is_video_playing(item['link']): # ভিডিও চেক
             active_links.append(item)
-            if len(active_links) == 1: # প্রথম সচল লিংকটি প্লেলিস্টে যাবে
+            if len(active_links) == 1:
                 m3u += f'#EXTINF:-1 tvg-logo="{item["logo"]}",{name}\n{item["link"]}\n'
-    
-    # সচল লিংক থাকলে ডাটাবেস আপডেট হবে, না থাকলে ওই চ্যানেলই ডিলিট হয়ে যাবে
     if active_links: new_db[name] = active_links
 
 with open('database.json', 'w', encoding='utf-8') as f:
